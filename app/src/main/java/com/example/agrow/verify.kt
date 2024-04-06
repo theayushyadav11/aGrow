13a829f1cd57b3c6b549ed4cb3cd009aca8c5a69
Ayush <theayushyadav11@gmail.com>
2024-04-06 12:50:19 +0530

    2 activity

--- app/src/main/java/com/example/agrow/verify.kt
+++ app/src/main/java/com/example/agrow/verify.kt
@@ -1,5 +1,7 @@
+
 package com.example.agrow
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,13 +14,23 @@ class verify : AppCompatActivity() {
     private lateinit var binding:ActivityVerifyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-enableEdgeToEdge()
+        enableEdgeToEdge()
         binding=ActivityVerifyBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
         binding.ivBack.setOnClickListener{
             onBackPressed()
         }
+        binding.tvPhone.setText(binding.tvPhone.text.toString()+intent.getStringExtra("phone"))
+        binding.btnVerify.setOnClickListener{
+            var otp=binding.pinOTP.text.toString()
+            if(otp.length==6)
+            {
+                val i= Intent(this,MainActivity::class.java)
+                startActivity(i)
+            }
+        }
+
 
 
     }
